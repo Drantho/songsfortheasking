44be6645505143b45fba9fbab539a5c58d4cc1ca
Anthony Mitchell <drantho@gmail.com>
2022-04-08 15:56:32 -0600

    bandlist styling added

--- src/pages/BandList.vue
+++ src/pages/BandList.vue
@@ -1,11 +1,55 @@
 <script setup>
-
+import { RouterLink } from "vue-router";
+import data from "app/newData";
 </script>
 
 <template>
-<h1>Band List</h1>
+  <h1>Band List</h1>
+
+  <ul>
+    <li v-for="band in data" :key="band.bandId">
+      <div class="q-pa-md">
+        <div class="row">
+          <div class="col">
+            <h2 class="title">
+              <RouterLink :to="`/band/${band.bandId}`">{{ band.band }}</RouterLink>
+            </h2>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col">
+            <q-img :src="require(`../assets/${band.image}`)" alt="" />
+          </div>
+          <div class="col info">
+            <p>
+              {{band.info.text}} - {{band.info.attribution}}
+            </p>
+            <q-btn color="primary" label="On to the tabs" :to="`/band/${band.bandId}`"/>
+          </div>
+        </div>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.q-pa-md {
+  padding: 0;
+}
+
+.col.info {
+  padding: 0 25px;
+}
+
+.title {
+  text-align: center;
+}
 
 </style>
